6971433f67ca82f75706295e3db717ce36e34cec
Taylor Mcgilton <taylor.mcgilton@bellevuecollege.edu>
2015-11-19 19:22:58 -0800

    First push of capitals asp program

---
+++ McGiltonCapitals/McGiltonCapitals/default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="McGiltonCapitals._default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Repeater ID="countryRepeater" runat="server">
+        <ItemTemplate>
+            Country ID: <%#Eval("CountryID") %><br />
+            Country: <%#Eval("Country") %><br />
+            Capital: <%#Eval("Capital") %><br /><br />
+        </ItemTemplate>
+    </asp:Repeater>
+
+    Enter Country ID to delete:
+
+
+
+    <asp:TextBox ID="TextBoxCountry" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="ButtonDelete" runat="server" OnClick="ButtonDelete_Click" Text="Button" />
+
+
+
+</asp:Content>
---
+++ McGiltonCapitals/McGiltonCapitals/default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace McGiltonCapitals
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            SqlDataReader reader;
+            string connectionString = ConfigurationManager.ConnectionStrings["CapitalCS"].ConnectionString;
+            conn = new SqlConnection(connectionString);
+            comm = new SqlCommand("SELECT CountryID, Country, Capital FROM CapitalsTable", conn);
+
+            conn.Open();
+            reader = comm.ExecuteReader();
+            countryRepeater.DataSource = reader;
+            countryRepeater.DataBind();
+            reader.Close();
+            conn.Close();
+        }
+
+        protected void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            string connectionString = ConfigurationManager.ConnectionStrings["CapitalCS"].ConnectionString;
+            conn = new SqlConnection(connectionString);
+            comm = new SqlCommand("DELETE FROM CapitalsTable WHERE CountryID = @CountryID", conn);
+            comm.Parameters.Add("@CountryID", System.Data.SqlDbType.Int);
+            comm.Parameters["@CountryID"].Value = TextBoxCountry.Text;
+
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+            Response.Redirect("default.aspx");
+
+        }
+    }
+}
